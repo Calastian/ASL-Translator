e77c2e8c5484b8a31ea3a758c97f11968c132c2d
Calastian <christianwyattnovgrod@gmail.com>
2025-10-29 19:00:48 -0400

    added a camera with both landmark and not also added the key.csv and the output.csv into github and my onehotencodding

---
+++ src/training/onehot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+df = pd.read_csv("../data/Finished_Output.csv")
+
+ohe = OneHotEncoder(handle_unknown='ignore', sparse_output= False)
+ohetransform = ohe.fit_transform(df[["Gloss"]].head()) #remove head later this is just for testing!
+feature_names = [name.replace('Gloss_', '') for name in ohe.get_feature_names_out(['Gloss'])]
+encoded_df = pd.DataFrame(ohetransform, columns=feature_names)
+
+print(encoded_df)
--- tests/testModel.py
+++ tests/testModel.py
@@ -3,6 +3,7 @@ import preprocess as pp
 pp.makePreProcessedData(glossCSVPath='../ASL_Citizen/splits/train.csv',
                         videoFolderPath='../ASL_Citizen/test_videos/',
                         outputCSVFilePath='processedData/output.csv',
-                        filterCSVPath='Key_ASL.csv')
+                        filterCSVPath='Key_ASL.csv',
+                        )
 
 
