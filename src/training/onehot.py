2478683dbebe22cbef97ca96506a9a947733b3b2
Calastian <christianwyattnovgrod@gmail.com>
2025-11-03 01:08:15 -0500

    addedd one hot ad csvs

--- src/training/onehot.py
+++ src/training/onehot.py
@@ -3,8 +3,9 @@ from sklearn.preprocessing import OneHotEncoder
 df = pd.read_csv("../data/Finished_Output.csv")
 
 ohe = OneHotEncoder(handle_unknown='ignore', sparse_output= False)
-ohetransform = ohe.fit_transform(df[["Gloss"]].head()) #remove head later this is just for testing!
+ohetransform = ohe.fit_transform(df[["Gloss"]]) #remove head later this is just for testing!
 feature_names = [name.replace('Gloss_', '') for name in ohe.get_feature_names_out(['Gloss'])]
 encoded_df = pd.DataFrame(ohetransform, columns=feature_names)
+df = df[["Video file"]].join(encoded_df)
 
-print(encoded_df)
+df.to_csv('onehot.csv')
